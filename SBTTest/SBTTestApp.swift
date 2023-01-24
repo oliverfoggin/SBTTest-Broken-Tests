0080c8dea7b844b4aa0e3852640c8f75fcccd2a2
Oliver Foggin <oliverfoggin@me.com>
2023-01-24 09:38:56 +0000

    Initial Commit

---
+++ SBTTest/SBTTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SBTTestApp.swift
+//  SBTTest
+//
+//  Created by Oliver Foggin on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SBTTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
